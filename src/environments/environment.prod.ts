16be7b347c64e11e466ed63d8742824e8fa75121
jabranemohamed <jabranemohamed@gmail.com>
2023-08-28 15:35:33 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'XXX',
+    appId: 'XXXX',
+    storageBucket: 'XXXXX',
+    apiKey: 'XXX',
+    authDomain: 'XXX',
+    messagingSenderId: 'XXX',
+    measurementId: 'XXX',
+  }
+};
